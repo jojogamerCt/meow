11ebea65c7714c704614029460e57cdfc0fa2f09
Daniel Vargas <41929135+qqqwda@users.noreply.github.com>
2024-04-01 16:27:10 -0300

    multi acc init

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import time
+import sys
 
-DISCORD_EMAIL = os.getenv('DISCORD_EMAIL')
-PASSWORD = os.getenv('PASSWORD')
-CHANNEL = os.getenv('CHANNEL')
+
 DRIVER_PATH = os.getenv('DRIVER_PATH')
 logger = Logger.getInstance().get_logger()
 
 if __name__ == "__main__":
     # Start the bot
     
+    email = os.getenv('EMAIL')
+    password = os.getenv('PASSWORD')
+    channel = os.getenv('CHANNEL')
+    api_key = os.getenv('API_KEY')
+    
+    
     main = Driver(DRIVER_PATH)
     logger.info("🚀 Starting bot!")
+    
+    if os.path.exists("catch_counter.json"):
+        os.remove("catch_counter.json")  
 
     main.start_driver()
     main.navigate_to_page("https://discord.com/login")
-    main.login(DISCORD_EMAIL,PASSWORD)
-    main.navigate_to_page(CHANNEL)
+    main.login(email,password)
+    main.navigate_to_page(channel)
     time.sleep(5)
     main.play()
     
---
+++ run_account2.bat
@@ -0,0 +1,12 @@
+pip install -r requirements.txt
+set EMAIL=cursedelboom02@gmail.com
+set PASSWORD=1324rt556
+set CHANNEL=https://discord.com/channels/1220056646437568582/1220056647087427666
+set API_KEY=3fe858485cmsh4b09dad8226ecc6p149f14jsn799ac3bc7fb1
+set ENABLE_AUTO_BUY_BALLS=True
+set ENABLE_AUTO_RELEASE_DUPLICATES=False
+set ENABLE_AUTO_EGG_HATCH=True
+set ENABLE_AUTO_LOOTBOX_OPEN=True
+set ENABLE_FISHING=True
+py main.py
+pause
