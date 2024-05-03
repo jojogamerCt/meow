8dad91b92797c43ceb5cd82368c48a88a822a41a
Daniel Vargas <41929135+qqqwda@users.noreply.github.com>
2024-05-02 23:58:06 -0400

    merge with develop and screenshot folder to gitignore, added run_account_example.bat

---
+++ run_account_example.bat
@@ -0,0 +1,19 @@
+pip install -r requirements.txt
+set SESSION_NAME=account1
+set EMAIL=email1@gmail.com
+set PASSWORD=strongpassword
+set CHANNEL=https://discord.com/channels/id/channel
+set API_KEY=RAPID-API-KEY
+set DISCORD_TOKEN=DISCORD-TOKEN
+set ENABLE_AUTO_BUY_BALLS=True
+set ENABLE_AUTO_RELEASE_DUPLICATES=False
+set ENABLE_AUTO_EGG_HATCH=True
+set ENABLE_AUTO_LOOTBOX_OPEN=True
+set ENABLE_AUTO_QUEST_REROLL=True
+set ENABLE_FISHING=False
+set ENABLE_BATTLE_NPC=False
+set ENABLE_HUNTING=True
+set ENABLE_CHROME_HEADLESS=False
+set ENABLE_RUN_PICTURES=True
+py main.py %0
+pause
